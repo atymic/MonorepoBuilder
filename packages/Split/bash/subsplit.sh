4a24b541db0ab003c1e22c52209d8e6a065f75b6
atymic <atymicq@gmail.com>
2019-11-08 10:38:04 +1100

    test

--- packages/Split/bash/subsplit.sh
+++ packages/Split/bash/subsplit.sh
@@ -121,7 +121,7 @@ function split_branch()
         echo " - syncing branch '${BRANCH}'"
         echo "git checkout -b ${LOCAL_BRANCH}-checkout ${BRANCH}"
         git branch
-        git checkout -b "${LOCAL_BRANCH}-checkout" "${BRANCH}" -vvv || die "Failed while git checkout branch '${BRANCH}'"
+        git checkout -b "${LOCAL_BRANCH}-checkout" "${BRANCH}" || die "Failed while git checkout branch '${BRANCH}'"
         git subtree split -q --prefix="$FROM_DIRECTORY" --branch="$LOCAL_BRANCH" "${BRANCH}" || die "Failed while git subtree split for '${BRANCH}'"
 
         git push -q --force origin ${LOCAL_BRANCH}:${BRANCH} || die "Failed pushing branch to remote repo"
